4929617a5f8101580d78abe6638f156f0ab5f98f
ReggieGrant <grantreggie09@gmail.com>
2025-08-19 19:44:51 -0700

    class2

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,34 @@
+let isVisible = true; // global variable to track visibility
 
-function hello() {
-    console.log("Hello There!");
-    goodbye();
+function toggleVisibility() {
+    if (isVisible) {
+        $("#form").fadeOut(); // hide the form section
+        isVisible = false; // update the visibility state
+    } else {
+        $("#form").fadeIn(); // show the form section
+        isVisible = true; // update the visibility state
+    }
 }
 
-function goodbye() {
-    console.log("Bye!");
+
+function saveTask() {
+    console.log("Hello im the saveTask");
+    // get the value of the input
+
+    // create a new task object
+
+    // add the task to the list
+
 }
 
 function init() {
     console.log("Hello im the init");
-    hello();
+    // hooks
+    $("#btnAdd").click(saveTask);
+    $("#btnDetails").click(toggleVisibility);
 }
 
+// new comment
+window.onload = init;// it waits until the html and css finish to run the logic
 
 
-
-window.onload = hello;// it waits until the html and css finish to run the logic 
